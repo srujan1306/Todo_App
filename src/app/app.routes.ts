f646b46b14ac13abddb393c6df1684a98e9b1d47
srujan1306 <srujan.pothu@proclink.com>
2024-09-11 10:16:13 +0530

    routing and fronted half done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TaskViewComponent } from './task-view/task-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: TaskViewComponent,
+  },
+];
