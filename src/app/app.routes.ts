f502a7fc7ef9f39e40509286a357077c544016f1
srujan1306 <srujan.pothu@proclink.com>
2024-09-12 17:08:19 +0530

    login and signup done successfully

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,10 @@ export const routes: Routes = [
     component: LoginComponent,
   },
   {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
     path: 'signup',
     component: SignupComponent,
   },
